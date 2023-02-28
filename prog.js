348175c067772d0a4173c6b7bc1bd74a336d7844
Raoufabderraouf <121226672+Raoufabderraouf@users.noreply.github.com>
2023-02-28 20:57:56 +0100

    Update prog.js

--- prog.js
+++ prog.js
@@ -7,7 +7,7 @@ console.log(lengthOfSentence)
 const words = sentence.split(" ").length
 console.log(words);
 
-const vowels = [`a`, `i`, `o`, `y`, `u`]
+const vowels = [`a`, `e`, `i`, `o`, `y`, `u`]
 
 let vowelsCount = 0
 for (let i = 0; i < sentence.length; i++){
